8b8bb90a7bea2ecedc0aef0c3933ab4b6f408f5f
Aleksander Grzybowski <kelog@orange.pl>
2017-03-21 18:59:18 +0100

    Base project structure

---
+++ src/main/java/songbook/SongbookApplication.java
@@ -0,0 +1,12 @@
+package songbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongbookApplication.class, args);
+	}
+}
