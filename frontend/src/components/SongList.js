4c9651424b5838a91d30d367284a5caa0960bc04
Aleksander Grzybowski <kelog@orange.pl>
2017-03-22 15:12:18 +0100

    Remove not needed spinner

--- frontend/src/components/SongList.js
+++ frontend/src/components/SongList.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
-import Spinner from 'react-spinkit';
 
 export default class SongList extends Component {
     render() {
-        if (this.props.requestInProgress) {
-            return <Spinner className="text-center" spinnerName='three-bounce'/>
-        }
-
         const items = this.props.songs.map(song => (
             <ListGroupItem key={song.id}>
                 <span>{song.title}</span>
