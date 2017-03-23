ca29fbc4302b55f3fb33026a88b90827a2f69647
Aleksander Grzybowski <kelog@orange.pl>
2017-03-23 18:04:39 +0100

    Add sorting

--- src/main/java/songbook/song/SongRepository.java
+++ src/main/java/songbook/song/SongRepository.java
@@ -1,6 +1,7 @@
 package songbook.song;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.Optional;
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
     List<Song> findByTitleIgnoreCaseContaining(String query);
+    
+    @Query("select s from Song s order by s.title asc")
+    List<Song> findAll();
     
     Optional<Song> findById(Long id);
 }
