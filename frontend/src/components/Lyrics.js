cdb34cc0956342cb4bc0f901e9f33103ed048637
Aleksander Grzybowski <kelog@orange.pl>
2017-03-26 14:43:01 +0200

    Edit and delete buttons

--- frontend/src/components/Lyrics.js
+++ frontend/src/components/Lyrics.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 const textStyle = {
     fontFamily: 'inherit',
     fontSize: '14px'
 };
 
-const Lyrics = ({isPresent, title, text}) => (
-    isPresent ? (
+const Lyrics = ({isPresent, title, text}) => {
+
+    const menuIcon = <span className="glyphicon glyphicon-option-vertical"/>;
+    const menu = (
+        <DropdownButton title={menuIcon} id="lyrics-menu">
+            <MenuItem>
+                <span className="glyphicon glyphicon-edit"/> Edit...
+            </MenuItem>
+            <MenuItem>
+                <span className="glyphicon glyphicon-trash"/> Delete...
+            </MenuItem>
+        </DropdownButton>
+    );
+
+    const main = (
         <div>
             <h1 className="text-center">
                 {title}
+                <div className="pull-right">
+
+                    {menu}
+                </div>
             </h1>
 
             <pre style={textStyle}>{text}</pre>
         </div>
-    ) : (
-        <div/>
-    )
-);
+    );
+
+    return isPresent ? main : <div/>;
+};
 
 export default Lyrics;
