ae6e1b55d1bb627d4cb6b2c3d4e2a4072f4adad3
Aleksander Grzybowski <kelog@orange.pl>
2017-04-02 19:45:06 +0200

    Add 'x' button to clear search input

--- frontend/src/components/SearchInput.js
+++ frontend/src/components/SearchInput.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup } from 'react-bootstrap';
 import { translate } from 'react-i18next';
 
 const SearchInput = ({text, inputChanged, t}) => (
     <h1 className="text-center">
         <FormGroup bsSize="large">
-            <FormControl
-                type="text"
-                placeholder={t('search') + '...'}
-                value={text}
-                onChange={e => inputChanged(e.target.value)}
-            />
+            <InputGroup bsSize="large">
+                <FormControl
+                    type="text"
+                    placeholder={t('search') + '...'}
+                    value={text}
+                    onChange={e => inputChanged(e.target.value)}
+                />
+                <InputGroup.Addon
+                    style={{cursor: 'pointer'}}
+                    onClick={e => inputChanged('')}
+                >
+                    <span>&times;</span>
+                </InputGroup.Addon>
+            </InputGroup>
         </FormGroup>
+
     </h1>
 );
 
